a89775d01702717c40c66b6f459513c46ecbdf12
danciu-valentin-nicolae <valentindanciu02@gmail.com>
2026-05-06 13:48:14 +0300

    added patch method to corsconfig for manual transaction approval to work

--- src/main/java/com/mdsproject/backend/config/CorsConfig.java
+++ src/main/java/com/mdsproject/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173") // Allow frontend origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
